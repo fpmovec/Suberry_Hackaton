e101913c5648d0beadd6edeb9802c48e222d7a68
DESKTOP-TVJJEQD\User <snodok16@gmail.com>
2024-02-03 11:16:51 +0300

    1 com

--- Syberry.Web/Program.cs
+++ Syberry.Web/Program.cs
@@ -1,4 +1,20 @@
+using System.Text.Json.Serialization;
+using Syberry.Web.Services.Abstractions;
+using Syberry.Web.Services.Implementations;
+
 var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddHttpClient("CommonFactory", _ => { })
+    .ConfigurePrimaryHttpMessageHandler(() => new HttpClientHandler
+    {
+        ServerCertificateCustomValidationCallback = (_, _, _, _) => true
+    });
+
+builder.Services.AddScoped<IBelarusBankService, BelarusBankService>();
+
+builder.Services.AddControllers().AddJsonOptions(x =>
+    x.JsonSerializerOptions.ReferenceHandler = ReferenceHandler.IgnoreCycles);
+
 
 // Add services to the container.
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
@@ -16,29 +32,6 @@ if (app.Environment.IsDevelopment())
 
 app.UseHttpsRedirection();
 
-var summaries = new[]
-{
-    "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
-};
-
-app.MapGet("/weatherforecast", () =>
-    {
-        var forecast = Enumerable.Range(1, 5).Select(index =>
-                new WeatherForecast
-                (
-                    DateOnly.FromDateTime(DateTime.Now.AddDays(index)),
-                    Random.Shared.Next(-20, 55),
-                    summaries[Random.Shared.Next(summaries.Length)]
-                ))
-            .ToArray();
-        return forecast;
-    })
-    .WithName("GetWeatherForecast")
-    .WithOpenApi();
+app.MapControllers();
 
 app.Run();
-
-record WeatherForecast(DateOnly Date, int TemperatureC, string? Summary)
-{
-    public int TemperatureF => 32 + (int) (TemperatureC / 0.5556);
-}
---
+++ Syberry.Web/Services/Abstractions/IBelarusBankService.cs
@@ -0,0 +1,8 @@
+﻿using Syberry.Web.Models;
+
+namespace Syberry.Web.Services.Abstractions;
+
+public interface IBelarusBankService
+{
+    Task<Rate> BelarusBankRates();
+}
