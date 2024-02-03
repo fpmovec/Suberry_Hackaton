b441aa4e8bc78ec479f0d08b2cebad44ce3326c0
fpmovec <10.maks.10K@gmail.com>
2024-02-03 17:36:24 +0300

    Final

--- Syberry.Telegram/Program.cs
+++ Syberry.Telegram/Program.cs
@@ -7,9 +7,9 @@ test.StartBotAsync();
 
 Console.WriteLine("Press ENTER to stop the bot");
 
-Rate banksInfo =
-    await ApiService.GetBanksInfo("BelarusBank", "USD", DateTime.Today);
-
-Console.WriteLine($"{banksInfo.SellRate} {banksInfo.BuyRate} {DateTime.Today}");   
+// Rate banksInfo =
+//     await ApiService.GetBanksInfo("BelarusBank", "USD", DateTime.Today);
+//
+// Console.WriteLine($"{banksInfo.SellRate} {banksInfo.BuyRate} {DateTime.Today}");   
 
 Console.ReadLine();
